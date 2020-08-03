1df194b5137080dcd71c51cef7dd747b682a90b9
Md towhid <newtowhid1232@gmail.com>
2020-08-03 20:17:52 +0600

    End of section 5

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
         console.log('logged in successfully');
       },
       (error) => {
-        console.log('logged in failed');
+        console.log(error);
       }
     );
   }
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
 
 namespace DatingApp.API
 {
@@ -54,7 +56,16 @@ namespace DatingApp.API
             }
             else
             {
-
+                // app.UseExceptionHandler(builder =>
+                // {
+                //     builder.Run(async context =>
+                //     {
+                //         context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                //         var error = context.Features.Get<IExceptionHandlerFeature>();
+                //         // if(error!=null)
+                //         //     await context.Response.StartAsync(error.Error.Message);
+                //     });
+                // });
             }
 
             // app.UseHttpsRedirection();
